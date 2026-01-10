6d534eb23f39f6d2af070ce8d904eeaa48c23e7a
Ilya Alekhin <alekhine@dfsystems.ru>
2026-01-10 14:25:38 +0300

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module PublicTelegramChannelParser
+
+go 1.25.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.11.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.47.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"slices"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/joho/godotenv"
+)
+
+type Scenario struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type UserResponse struct {
+	Status    string     `json:"status"`
+	Scenarios []Scenario `json:"scenarios"`
+}
+
+type ActionsResponse struct {
+	Status string `json:"status"`
+}
+
+func sendIotRequest(url string, method string) []byte {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		panic(err)
+	}
+	token := os.Getenv("YANDEX_TOKEN")
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		log.Fatalf("Error sending IOT request: %d %s", resp.StatusCode, string(body))
+	}
+
+	return body
+}
+
+func getScenarioID() string {
+	bodyBytes := sendIotRequest("https://api.iot.yandex.net/v1.0/user/info", "GET")
+
+	var result UserResponse
+	err := json.Unmarshal(bodyBytes, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	if result.Status != "ok" {
+		log.Fatalf("Failed to get scenario id: %s", result.Status)
+	}
+
+	for _, scenario := range result.Scenarios {
+		if scenario.Name == os.Getenv("SCENARIO_NAME") {
+			return scenario.ID
+		}
+	}
+	return ""
+}
+
+func startScenario(scenarioID string) {
+	url := fmt.Sprintf("https://api.iot.yandex.net/v1.0/scenarios/%s/actions", scenarioID)
+	bodyBytes := sendIotRequest(url, "POST")
+
+	var result ActionsResponse
+	err := json.Unmarshal(bodyBytes, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	if result.Status != "ok" {
+		log.Fatalf("Failed to start scenario: %s", result.Status)
+	}
+	log.Printf("Scenario %s started", scenarioID)
+}
+
+func checkTelegramChannel() {
+	log.Printf("Checking channel")
+	channelUrl := os.Getenv("CHANNEL_URL")
+	res, err := http.Get(channelUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	triggerWords := strings.Split(os.Getenv("TRIGGER_WORDS"), ",")
+	log.Printf("Checking trigger words: %s", triggerWords)
+	if triggerWords[0] == "" {
+		log.Printf("No trigger words found")
+		return
+	}
+
+	element := doc.Find(".tgme_widget_message_text").Last()
+	log.Println("Last element in channel:", element.Text())
+	found := slices.ContainsFunc(triggerWords, func(triggerWord string) bool {
+		return strings.Contains(strings.ToLower(element.Text()), triggerWord)
+	})
+
+	if found {
+		sendDangerousNotification()
+	}
+	log.Println("Check channel complete")
+}
+
+func sendDangerousNotification() {
+	scenarioId := getScenarioID()
+	if scenarioId == "" {
+		log.Fatal("Scenario id not found")
+	} else {
+		log.Printf("Found scenario id: %s", scenarioId)
+	}
+	startScenario(scenarioId)
+	time.Sleep(5 * time.Second)
+	startScenario(scenarioId)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+		log.Println("\nShutting down gracefully...")
+		cancel()
+	}()
+
+	envSleepTime, exists := os.LookupEnv("PARSING_SLEEP_TIME")
+	if !exists {
+		envSleepTime = "30"
+	}
+	sleepTime, _ := strconv.Atoi(envSleepTime)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			checkTelegramChannel()
+			log.Printf("Sleeping for %d seconds", sleepTime)
+			time.Sleep(time.Duration(sleepTime) * time.Second)
+		}
+	}
+}
